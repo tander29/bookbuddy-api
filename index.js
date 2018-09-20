69528a65ccab453e411f1b87e72c459d3ca4090b
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2018-09-20 11:05:57 -0400

    bugs

--- index.js
+++ index.js
@@ -12,17 +12,12 @@ app.set("port", process.env.PORT || 3000);
 app.use(morgan("tiny"));
 app.use(express.json());
 
-
-const client = new Client({
-    connectionString: process.env.DATABASE_URL || 5000,
-    ssl: true,
- });
  
  app.get('/login', (req, res) => {
     res.send({ stuff: 'vfgkj,ecd' })
  });
  
- app.listen(5000, () => {
+ app.listen(3000, () => {
     console.log('app listening on port 3000!')
  });
- client.connect();
+ 
