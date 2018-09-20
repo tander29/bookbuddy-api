2180aa87f9003ad298dbc6f04d3b3660d7489a29
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2018-09-20 10:44:03 -0400

    first connect

--- index.js
+++ index.js
@@ -5,23 +5,25 @@ const cors = require("cors");
 const { Client } = require('pg');
 const { User, sequelize } = require("./models");
 
-
 const app = express();
+
+app.set("port", process.env.PORT || 3000);
 
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
 
+
 const client = new Client({
-  connectionString: process.env.DATABASE_URL || 3000,
-  ssl: true,
-});
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
-
-app.listen(3000, () => {
+    connectionString: process.env.DATABASE_URL || 5000,
+    ssl: true,
+ });
+ 
+ app.get('/login', (req, res) => {
+    res.send({ stuff: 'vfgkj,ecd' })
+ });
+ 
+ app.listen(5000, () => {
     console.log('app listening on port 3000!')
-});
-client.connect();
+ });
+ client.connect();
