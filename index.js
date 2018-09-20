5bdab85bd34054a859932730de02db702ca749f0
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2018-09-20 11:16:04 -0400

    fix

--- index.js
+++ index.js
@@ -12,10 +12,6 @@ app.set("port", process.env.PORT || 5000);
 app.use(morgan("tiny"));
 app.use(express.json());
 
-const client = new Client({
-    connectionString: process.env.DATABASE_URL || 5000,
-    ssl: true,
-});
 
 app.get('/login', (req, res) => {
     res.send({ stuff: 'vfgkj,ecd' })
@@ -24,5 +20,5 @@ app.get('/login', (req, res) => {
 app.listen(5000, () => {
     console.log('app listening on port 3000!')
 });
-client.connect();
+
 
