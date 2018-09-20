68e79b526bab35d03f3428e882abb7667bcecbf8
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2018-09-20 13:08:49 -0400

    heroku

--- index.js
+++ index.js
@@ -2,23 +2,19 @@ require("dotenv-safe").config({ allowEmptyValues: true });
 const express = require('express')
 const morgan = require("morgan");
 const { Client } = require('pg');
-const { User, sequelize } = require("./Models");
 
 const app = express();
 
 app.set("port", process.env.PORT || 5000);
 
-
 app.use(morgan("tiny"));
 app.use(express.json());
 
-
-app.get('/login', (req, res) => {
+ app.get('/login', (req, res) => {
     res.send({ stuff: 'vfgkj,ecd' })
+ });
+
+
+app.listen(app.get("port"), () => {
+    console.log('app listening on port 5000!')
 });
-
-app.listen(5000, () => {
-    console.log('app listening on port 3000!')
-});
-
-
