6680b16c22e88655206318c4a8c085d62642c458
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2018-09-26 13:55:43 -0400

    heroku

--- Controllers/books.js
+++ Controllers/books.js
@@ -2,12 +2,21 @@ const express = require('express');
 const router = express.Router();
 const Sequelize = require('sequelize')
 
+//cpmstatadsfgasgfa
 
 const {books} = require('../Models');
 
-router.post('/', (req, res) => {    
-            res.json({stuff: "please fucking work"
-            })
+router.post('/', (req, res) => {
+    const {title, author} =
+    books.create(
+           title,
+           author
+        )
+        .then(book =>
+            res.json({
+                title: book.get('title'),
+                author: book.get('author')
+            }))
 })
 
 module.exports = router;
