f8803d037709033e36f2236e07e7dba9f3946e48
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2018-09-26 13:10:44 -0400

    books

--- Controllers/books.js
+++ Controllers/books.js
@@ -1,31 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const Sequelize = require('sequelize')
 
-const {books} = require('../Models');
 
-router.post('/', (req, res) =>{
-    const {title, author, isbn10, isbn13}
+const {
+    books
+} = require('../Models');
 
+router.post('/', (req, res) => {
     books.create(
-        Object.assign({}, req.body, {
-            userId: req.user.get("id")
-        }),
-        title,
-        author,
-        isbn10,
-        isbn13
-    )
-    })
-        .then(book => 
+            Object.assign({}, req.body, {
+                userId: req.user.get("id")
+            })
+        )
+        .then(book =>
             res.json({
-                title: book.get('title'),
-                author: book.get('author'),
-                isbn10: user.get('isbn10'),
-                isbn13: user.get('isbn13')
+                book: book
             }))
+})
 
-
-module.exports={
+module.exports = {
     router
 }
-
