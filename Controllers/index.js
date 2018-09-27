2a190582048003f88d2ee71a15881f380adb404c
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2018-09-27 15:02:49 -0400

    heroku

--- Controllers/index.js
+++ Controllers/index.js
@@ -1,10 +1,11 @@
 const user = require("./User");
 const { router } = require("./authorize");
 const books = require("./books")
-
+const message = require('./message')
 
 module.exports = {
   authorize: router,
   books,
+  message,
   user
 };
