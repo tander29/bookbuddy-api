03d09b140dd2d858be5d00f8d4e86a0204d6c246
Travis Anderson <thetanderson@gmail.com>
2018-09-28 14:00:46 -0400

    new fields in Messages table to and from user

---
+++ migrations/20180928175545-addtoandfromfieldstomessages.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.sequelize.query(
+      `
+      ALTER TABLE "Messages" ADD COLUMN toUserID VARCHAR(255)
+      ALTER TABLE "Messages" ADD COLUMN fromUserID VARCHAR(255)
+      `
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
