75ff1452f91c59e6f6259527ca2678b1a3cb08af
Travis Anderson <thetanderson@gmail.com>
2018-09-29 21:18:14 -0400

    looking to download to have the hasmanystuff

--- Controllers/User.js
+++ Controllers/User.js
@@ -5,17 +5,25 @@ const { User, books } = require("../Models");
 const router = express.Router();
 
 router.get("/:id", (req, res) => {
-    const id = req.params.id;
-    User.findById(id, {
+  const id = req.params.id;
+  User.findById(id, {
 
-      include: [
-        {
-          model: books
-        }
-      ]
-      
-    }).then(user => res.json({ user }));
-  });
+    include: [
+      {
+        model: books
+      }
+    ]
+
+  }).then(user => res.json({ user }));
+});
+
+// router.get("/all", (req, res) => {
+//   User.findAll({
+//     limit: req.query.limit || 50,
+//     offset: req.query.offset || 0,
+//     order: [["createdAt", "DESC"]]
+//   }).then(user => res.json({ oh: "my" }));
+// });
 
 
 
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const controllers = require("./Controllers")
 const app = express();
 
-app.set("port", process.env.PORT || 8000);
+app.set("port", process.env.PORT || 8001);
 
 app.use(morgan("tiny"));
 app.use(express.json());
@@ -17,9 +17,11 @@ app.get('/', (req, res) => {
     res.send('Heroku Server is runningx')
 });
 
+
 app.use("/authorize", controllers.authorize);
 app.use("/books", controllers.books)
 app.use("/message", controllers.message)
+// app.use("/user", controllers.user)
 
 app.listen(app.get("port"), () => {
     console.log(`app listening on port ${app.get("port")}`)
